2f3f15985660d358a7949a3c35242a24891aeace
cmlee-danbi <cm.lee@danbicorp.com>
2022-06-24 11:28:56 +0900

    device_preview, change_app_package_name 추가, desktop 에서 마우스로 drag 기능 활성화

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,41 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
 }
+
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isDesktop = !Platform.isAndroid || Platform.isIOS;
+
     return MaterialApp(
+      // Enable drag on desktop
+      scrollBehavior: isDesktop ? MyCustomScrollBehavior() : null,
+      // DevicePreview options
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      // Etc..
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
